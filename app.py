f4f19187abb4a6bcd7062d6f96d2a7ff50f05f3d
ShuqiZi <zsqrachel@outlook.com>
2021-10-22 23:42:18 +0100

    Line17

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-# HOME PAGE VIEW
+# HOME PAGE VIEW VIEW VIEW VIEW VIEW
 @app.route('/')
 def index():
     return render_template('index.html')
